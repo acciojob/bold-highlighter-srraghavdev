bae7962f093cd3726dde83ff7ed7a19a65feb6f7
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 02:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function highlight() {
 	const children= document.getElementsByTagName("BODY")[0].children
 	let pt = children[4]
-	console.log(pt)
 	let strong = pt.children
 	for(let i=0;i<strong.length;i++){
 		strong[i].style.color="green"
@@ -12,7 +11,6 @@ function highlight() {
 function return_normal() {
     const children= document.getElementsByTagName("BODY")[0].children
 	let pt = children[4]
-	console.log(pt)
 	let strong = pt.children
 	for(let i=0;i<strong.length;i++){
 		strong[i].style.color="black"
