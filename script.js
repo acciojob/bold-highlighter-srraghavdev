7b2f657de1cfe40acdde57b2d75fec0893cbe743
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 14:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function highlight() {
 	const c= document.body.children
-	var strong = c[4].children
+	console.log(c)
+	var strong = c[c.length-3].children
 	 for(let i=0;i<strong.length;i++){
 		strong[i].style.color="green"
 	}
@@ -8,7 +9,7 @@ function highlight() {
 
 function return_normal() {
     const c= document.body.children
-	var strong = c[4].children
+	var strong = c[c.length-3].children
 	for(let i=0;i<strong.length;i++){
 		strong[i].style.color="black"
 	}
